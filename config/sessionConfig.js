a31865d4a593600b60cf63f2431810c6e3d880bd
Godwin Chukwuebuka Igwegbe <godwincliff10@email.com>
2025-05-22 19:56:43 +0100

    PostgreSQL

--- config/sessionConfig.js
+++ config/sessionConfig.js
@@ -3,23 +3,31 @@ const session = require('express-session');
 const pgSession = require('connect-pg-simple')(session);
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+const { Pool } = require("pg");
+
+const pgPool = new Pool({
+  connectionString: process.env.DATABASE_URL, // from .env or Render
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const sessionMiddleware = session({
   store: new pgSession({
-    conString: process.env.DATABASE_URL,
-    tableName: 'session',
-    createTableIfMissing: !isProduction,
+    pool: pgPool,
+    tableName: "session", // default name
+    createTableIfMissing: true, // ✅ this creates the table if not found
   }),
-  secret: process.env.SESSION_SECRET || 'super-secret',
+  secret: process.env.SESSION_SECRET || "super-secret-key", // keep this in env
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: isProduction,
-    httpOnly: true,
-    sameSite: isProduction ? 'none' : 'lax',
-    maxAge: 1000 * 60 * 60 * 24,
+    secure: true, // required for HTTPS
+    sameSite: "none", // works with cross-origin
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
   },
 });
 
