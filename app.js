15a9341da9eece58ad6de98c426c2b554b3cb91a
Godwin Chukwuebuka Igwegbe <godwincliff10@email.com>
2025-05-22 13:45:41 +0100

    Port

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ app.use("/api/v1/auth", authRoute);
 
 
 // ✅ Server listen
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
